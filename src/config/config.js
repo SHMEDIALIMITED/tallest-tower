af0e50b4e381fbce42aa82e26f71136e6121c5a4
Patrick Wolleb <patrick.wolleb@shmedia.co.uk>
2013-03-10 18:07:45 +0000

    production db config

--- src/config/config.js
+++ src/config/config.js
@@ -26,6 +26,16 @@ module.exports = {
 
     }
   , production: {
+      root: require('path').normalize(__dirname + '/..'),
+      app: {
+        name: 'Tallest Tower Dev'
+      },
+      db: process.env.MONGOLAB_URI,
+      facebook: {
+          clientID: "490996157610487"
+        , clientSecret: "eb96c08874d3e31a1440a941dabe0c6a"
+        , callbackURL: "http://localhost:3000/auth/facebook/callback"
+      },
 
     }
 }
